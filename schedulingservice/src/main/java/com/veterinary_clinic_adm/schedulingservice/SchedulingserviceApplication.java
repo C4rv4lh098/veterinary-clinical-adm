16451c2d65a448781966938e766d71cd87dc4d33
raphaela araujo <rph.goes@gmail.com>
2021-07-03 20:25:12 -0300

    Criando api em java com a framework springbootpara o microsserviço de agendamento

---
+++ schedulingservice/src/main/java/com/veterinary_clinic_adm/schedulingservice/SchedulingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.veterinary_clinic_adm.schedulingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingserviceApplication.class, args);
+	}
+
+}
---
+++ schedulingservice/src/main/java/com/veterinary_clinic_adm/schedulingservice/model/Agenda.java
@@ -0,0 +1,101 @@
+package com.veterinary_clinic_adm.schedulingservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "consulta")
+public class Agenda {
+    private long id;
+    private String tutorName;
+    private String animalName;
+    private String date;
+    private String startHour;
+    private String endHour;
+
+    public Agenda(){
+
+    }
+
+    public Agenda(long id, String tutorName, String animalName, String date, String startHour, String endHour) {
+        this.id = id;
+
+        this.tutorName = tutorName;
+        this.animalName = animalName;
+        this.date = date;
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "tutorName", nullable = false)
+    public String getTutorName() {
+        return tutorName;
+    }
+
+    public void setTutorName(String tutorName) {
+        this.tutorName = tutorName;
+    }
+
+    @Column(name = "animalName", nullable = false)
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    @Column(name = "date", nullable = false)
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Column(name = "startHour", nullable = false)
+    public String getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
+    @Column(name = "endHour", nullable = false)
+    public String getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(String endHour) {
+        this.endHour = endHour;
+    }
+
+    @Override
+    public String toString() {
+        return "Agenda{" +
+                "id=" + id +
+                ", tutorName='" + tutorName + '\'' +
+                ", animalName='" + animalName + '\'' +
+                ", date='" + date + '\'' +
+                ", startHour='" + startHour + '\'' +
+                ", endHour='" + endHour + '\'' +
+                '}';
+    }
+}
