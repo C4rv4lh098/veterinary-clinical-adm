97b3112bb17ace1d5751a080d093a3dde1bc80bc
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2021-07-05 12:02:49 -0400

    Adicionando os diretorios para cada parte do sistema, adiçao do front-end base e configurando as rotas pelo Angular CLI #4

--- veterinary-clinic/src/app/app.module.ts
+++ veterinary-clinic/src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgendamentoComponent } from '../app/agendamento/agendamento.component';
+import { CadastroComponent } from '../app/cadastro/cadastro.component';
+import { ConsultaComponent } from '../app/consulta/consulta.component';
+import { HomeComponent } from '../app/home/home.component';
+import { HeaderComponent } from '../app/header/header.component';
+import { MenubarComponent } from '../app/menubar/menubar.component';
+import { CalendarComponent } from '../app/agendamento/calendar/calendar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendamentoComponent,
+    CadastroComponent,
+    ConsultaComponent,
+    HomeComponent,
+    HeaderComponent,
+    MenubarComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
