74e44d7a1b0bbaf9725afcf5f93b6e45ec6d31f5
vicsenedesse <engsoftvicsenedesse@gmail.com>
2021-07-07 16:08:47 -0400

    modificaçoes #2

--- agendamento/src/main/java/com/agendamento/agendamento/AgendamentoApplication.java
+++ agendamento/src/main/java/com/agendamento/agendamento/AgendamentoApplication.java
@@ -3,8 +3,8 @@ package com.agendamento.agendamento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = 'com.agendamento.agendamento')
-@EntityScan(scanBasePackages = "com.agendamento.agendamento.model")
+@SpringBootApplication(scanBasePackages = "com.agendamento.agendamento")
+// @EntityScan(scanBasePackages = "com.agendamento.agendamento.model")
 public class AgendamentoApplication {
 
 	public static void main(String[] args) {
