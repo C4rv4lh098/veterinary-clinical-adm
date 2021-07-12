2fcbb351be91fe9c7c0b9dc61c4f544c163b6d40
vicsenedesse <engsoftvicsenedesse@gmail.com>
2021-07-12 12:49:52 -0400

    Atualizaçoes api cadastro #1

---
+++ registerservice/src/test/java/com/veterinary_clinic_adm/registerservice/registerservice/RegisterserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinary_clinic_adm.registerservice.registerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
